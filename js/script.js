99f8d17e97d8580890eb25025c393695722af58f
ViktorMo <34257555+Mo-Love@users.noreply.github.com>
2025-10-21 18:24:20 +0300

    Add functions to play YouTube video and color page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-
+function playSong(videoId) {
+    // Заміни на реальний ID з твого YouTube
+    window.open('https://www.youtube.com/watch?v=' + videoId, '_blank');
+}
+function colorPage(page) {
+    alert('Режим розмальовки для сторінки ' + page + '! (Додай Canvas пізніше для малювання.)');
+}
